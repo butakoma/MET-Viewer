42599f9221a82244043fe6d458fd4d89f2c0aa42
butakoma <ky.tanabota@gmail.com>
2019-06-29 19:02:29 +0900

    add departments menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,24 @@
 <template>
  <nav class="navbar">
      <div class="container">
-         <nuxt-link to="/" class="navbar-item">MET-Viewer</nuxt-link>
+         <div class="navbar-menu">
+             <nuxt-link to="/" class="navbar-item">MET-Viewer</nuxt-link>
+             <div class="navbar-item has-dropdown is-hoverable">
+                 <nuxt-link to="/" class="navbar-link">departments</nuxt-link>
+                 <div class="navbar-dropdown">
+                     <nuxt-link to="/departments/asia" class="navbar-item">
+                       <span>
+                           <strong>Asian Art</strong>
+                       </span>
+                     </nuxt-link>
+                     <nuxt-link to="/departments/egypt" class="navbar-item">
+                       <span>
+                           <strong>Egyptian Art</strong>
+                       </span>
+                     </nuxt-link>
+                 </div>
+             </div>
+         </div>
      </div>
  </nav>
 </template>
---
+++ pages/departments/egypt.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container">
+    <div>
+      <div>
+        <img v-bind:src="results.primaryImageSmall">
+      </div>
+      <div>
+        <span>
+          {{ results.title }}
+        </span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const departmentUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=10'
+    const objectIdData = await app.$axios.$get(departmentUrl)
+    const objectIds = objectIdData.objectIDs
+    const index = Math.floor(Math.random() * objectIds.length)
+    const objectId = objectIds[index]
+    const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
+    const data = await app.$axios.$get(url + objectId)
+    return { results: data }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
